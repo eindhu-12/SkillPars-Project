3c73a0aa5f86541d75dc12c0bcd467f2112722f0
unknown <rapoluindhu2929@gmail.com>
2025-02-28 11:05:36 +0530

    SkillPars

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
-      Home is here
+      
+      <Link to={"/signup"}>SignUp</Link><br />
+      <Link to={"/login"}>Login</Link>
     </div>
   )
 }
